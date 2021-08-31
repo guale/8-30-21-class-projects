b0d464b33cb5a9a10362bfdad0f2cdc07914d0ce
guale <gualeshadow@gmail.com>
2021-08-31 11:20:35 -0500

    add edit delete functionality

--- week1/day1/my-movies/src/components/Collection.js
+++ week1/day1/my-movies/src/components/Collection.js
@@ -1,23 +1,61 @@
 import React, { Component } from "react";
 import "../css/Collection.css";
-import Movies from "./Movie";
+import Movie from "./Movie";
 
 class Collection extends Component {
 	constructor() {
 		super();
+		this.state = {
+			movies: [],
+		};
+	}
+
+	addMovie() {
+		let movies = this.state.movies;
+		movies.push({ id: Date.now() });
+		this.setState({ movies });
+	}
+
+	deleteMovie(id) {
+		let newMovieArr = this.state.movies;
+		newMovieArr.map((movie, index) => {
+			if ((id === movie.id)) {
+				newMovieArr.splice(index, 1);
+			}
+		});
+
+		this.setState({ movies: newMovieArr });
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="container">
 				<div className="div-collection">
 					<div className="row">
-						<Movies/>
+						{this.state.movies.map((movies) => (
+							<Movie
+								key={movies.id}
+								id={movies.id}
+								title={movies.title}
+								year={movies.year}
+								genre={movies.genre}
+								desc={movies.desc}
+								deleteHandler={this.deleteMovie.bind(this)}
+							/>
+						))}
 					</div>
+				</div>
+				<div>
+					<button
+						onClick={this.addMovie.bind(this)}
+						className="btn btn-success add-button"
+					>
+						Add Movie
+					</button>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default Collection
+export default Collection;
--- week1/day1/my-movies/src/components/Movie.js
+++ week1/day1/my-movies/src/components/Movie.js
@@ -2,24 +2,130 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../css/Movie.css";
 
+const GENERIC_MOVIE_TITLE = "New Movie";
+const GENERIC_MOVIE_YEAR = 0;
+const GENERIC_MOVIE_GENRE = "Genre";
+const GENERIC_MOVIE_DESC = "Description";
+
 class Movie extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
+		this.titleContent = React.createRef();
+		this.yearContent = React.createRef();
+		this.genreContent = React.createRef();
+		this.descContent = React.createRef();
+
+		this.state = {
+			title: GENERIC_MOVIE_TITLE,
+			year: GENERIC_MOVIE_YEAR,
+			genre: GENERIC_MOVIE_GENRE,
+			desc: GENERIC_MOVIE_DESC,
+			editMode: false,
+		};
+	}
+
+	handleEdit() {
+		this.setState({ editMode: true });
+	}
+	handleSave() {
+		this.setState({
+			title: this.titleContent.current.value,
+			year: this.yearContent.current.value,
+			genre: this.genreContent.current.value,
+			desc: this.descContent.current.value,
+			editMode: false,
+		});
+	}
+	handleDelete() {
+		this.props.deleteHandler(this.props.id);
 	}
 
 	render() {
+		let titleElement;
+		let yearElement;
+		let genreElement;
+		let descElement;
+		let buttonArea;
+
+		if (this.state.editMode) {
+			titleElement = (
+				<input
+					ref={this.titleContent}
+					className="title-input edit-input"
+					defaultValue={this.state.title}
+				/>
+			);
+			yearElement = (
+				<input
+					ref={this.yearContent}
+					className="year-input edit-input"
+					defaultValue={this.state.year}
+				/>
+			);
+			genreElement = (
+				<input
+					ref={this.genreContent}
+					className="genre-input edit-input"
+					defaultValue={this.state.genre}
+				/>
+			);
+			descElement = (
+				<textarea
+					ref={this.descContent}
+					className="desc-textarea edit-input"
+					defaultValue={this.state.desc}
+				/>
+			);
+			buttonArea = (
+				<div>
+					<button
+						className="btn btn-primary"
+						onClick={this.handleSave.bind(this)}
+					>
+						Save
+					</button>
+				</div>
+			);
+		} else {
+			titleElement = <h5 className="card-title">{this.state.title}</h5>;
+			yearElement = <span>{this.state.year};</span>;
+			genreElement = <span>{this.state.genre}</span>;
+			descElement = <span>{this.state.desc}</span>;
+			buttonArea = (
+				<div>
+					<button
+						onClick={this.handleEdit.bind(this)}
+						className="btn btn-info"
+					>
+						Edit
+					</button>
+					<button
+						onClick={this.handleDelete.bind(this)}
+						className="btn btn-danger"
+					>
+						Delete
+					</button>
+				</div>
+			);
+		}
+
 		return (
 			<div className="col-sm-6">
 				<div className="card-view">
 					<div className="card">
-						<div className="card-header">
-							<h5 className="card-title">{this.props.title}</h5>
-						</div>
+						<div className="card-header">{titleElement}</div>
 						<ul class="list-group list-group-flush">
-							<li className="list-group-item">Year: {this.props.year}</li>
-							<li className="list-group-item">Genre: {this.props.genre}</li>
-							<li className="list-group-item">Description: {this.props.desc}</li>
+							<li className="list-group-item">
+								Year: {yearElement}
+							</li>
+							<li className="list-group-item">
+								Genre: {genreElement}
+							</li>
+							<li className="list-group-item">
+								Description: {descElement}
+							</li>
 						</ul>
+						<div className="card-footer">{buttonArea}</div>
 					</div>
 				</div>
 			</div>
