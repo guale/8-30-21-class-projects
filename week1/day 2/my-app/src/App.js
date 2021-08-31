079ed62a871823daad98fbc8a53be08f529a2c66
guale <gualeshadow@gmail.com>
2021-08-31 08:18:19 -0500

    week 1 day 2 first commit

---
+++ week1/day 2/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state={
+      name: "Bart",
+    }
+  }
+
+}
+
+export default App;
