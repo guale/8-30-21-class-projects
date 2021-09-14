4d11453b7108bfdfcd86f70c56da8a85400243bf
guale <gualeshadow@gmail.com>
2021-09-14 09:34:04 -0500

    week2 day2 finish

--- week3/day2/context-demo/src/App.js
+++ week3/day2/context-demo/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from "./Image";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+		<Image />
     </div>
   );
 }
