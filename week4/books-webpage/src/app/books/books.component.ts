917d7616c566139651d6e79eebb715e33621daf8
guale <gualeshadow@gmail.com>
2021-09-20 11:36:08 -0500

    finish day 1 requirements

--- week4/books-webpage/src/app/books/books.component.ts
+++ week4/books-webpage/src/app/books/books.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Book } from '../book';
+import { BOOKS } from 'src/mock-books';
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  books = BOOKS;
 
-  constructor() { }
+  selectedBook?: Book;
 
-  ngOnInit(): void {
+  blankBook: Book =   {
+    title: '',
+    author: '',
+    year: 0,
+    genre: '',
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.books.sort((a, b) => a.title.localeCompare(b.title));
+    this.books.sort((a, b) => a.author.localeCompare(b.author));
+  }
+
+  onSelect(book: Book): void {
+    if (this.selectedBook === book) {
+      this.selectedBook = this.blankBook;
+    } else {
+      this.selectedBook = book;
+    }
+  }
 }
---
+++ week4/books-webpage/src/mock-books.ts
@@ -0,0 +1,58 @@
+import { Book } from './app/book';
+
+export const BOOKS: Book[] = [
+  {
+    title: 'Song of Achilles, The',
+    author: 'Miller, Madeline',
+    year: 2012,
+    genre: 'Fantasy',
+  },
+  {
+    title: 'Circe',
+    author: 'Miller, Madeline',
+    year: 2018,
+    genre: 'Fantasy',
+  },
+  {
+    title: 'Wicked',
+    author: 'Maguire, Gregory',
+    year: 1995,
+    genre: 'Fantasy',
+  },
+  {
+    title: 'House of Leaves',
+    author: 'Danielewski, Mark Z.',
+    year: 2000,
+    genre: 'Horror',
+  },
+  {
+    title: 'Slaughterhouse Five',
+    author: 'Vonnegut, Kurt, Jr.',
+    year: 1969,
+    genre: 'Science Fiction',
+  },
+  {
+    title: 'Watchmen',
+    author: 'Moore, Alan',
+    year: 1987,
+    genre: 'Science Fiction',
+  },
+  {
+    title: 'Nausicaä of the Valley of the Wind: The Complete Series',
+    author: 'Miyazaki Hayao (宮崎 駿)',
+    year: 1995,
+    genre: 'Science Fiction',
+  },
+  {
+    title: 'Looking for Alaska',
+    author: 'Green, John',
+    year: 2005,
+    genre: 'Young Adult Fiction',
+  },
+  {
+    title: 'Scott Pilgrim 6 Books Collection Set',
+    author: 'O\'Malley, Byran Lee',
+    year: 2010,
+    genre: 'Graphic Novel',
+  },
+];
