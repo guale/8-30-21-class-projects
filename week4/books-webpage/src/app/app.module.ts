ab098aedc0991cf3a35743ebd2830c329f2542e6
guale <gualeshadow@gmail.com>
2021-09-20 10:07:34 -0500

    book project first commit

---
+++ week4/books-webpage/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
