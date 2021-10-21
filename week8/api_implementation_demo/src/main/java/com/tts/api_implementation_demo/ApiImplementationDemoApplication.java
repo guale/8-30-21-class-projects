0c84fa71274ffc520c650b86a4d1a5968ccb8a4a
guale <gualeshadow@gmail.com>
2021-10-21 08:56:29 -0500

    api first commit

---
+++ week8/api_implementation_demo/src/main/java/com/tts/api_implementation_demo/ApiImplementationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tts.api_implementation_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImplementationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiImplementationDemoApplication.class, args);
+	}
+
+}
---
+++ week8/api_implementation_demo/src/test/java/com/tts/api_implementation_demo/ApiImplementationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.api_implementation_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImplementationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
